ed537fcad0b71904123d43192fffb08fc976542a
Eduardo Santos De Assis <20242STADS0012@cefetes.br>
2025-10-16 14:37:22 -0300

    nem sei mais tlgd

--- frontend/templates/header.php
+++ frontend/templates/header.php
@@ -16,7 +16,7 @@ $currentFile = basename($_SERVER['PHP_SELF']);
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container">
-            <a class="navbar-brand" href="/frontend/index.php">IFES Movie DB</a>
+            <a class="navbar-brand" href="/index.php">IFES Movie DB</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
